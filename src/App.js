299f49e65f493fe245b53f5287a22405a1fd1894
MauEscalante <Escalantemauricio2002@gmail.com>
2025-06-28 19:23:32 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./NavBar";
-import Home from "./Home";
-import Login from "./Login";
-import ForgotPassword from "./ForgotPassword";
-import Register from "./Register";
+import Navbar from "./Components/NavBar";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import ForgotPassword from "./Components/ForgotPassword";
+import Register from "./Components/Register";
 
 const App = () => {
     return (
